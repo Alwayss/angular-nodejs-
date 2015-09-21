a1a3ba0d64c8182c42f2a4d2fb22b0fd7b04b595
wjhuang93@163.com <wjhuang93@163.com>
2015-09-21 15:57:20 +0800

    添加数据库表的注释

--- models/schema.js
+++ models/schema.js
@@ -7,41 +7,41 @@ module.exports={
 	user:{                 //用户表的基本属性字段
 		username: {type: String, unique: true},   //用户名
 	    password: {type: String, select: false},   //密码
-	    telphone: {type:Number},
+	    telphone: {type:Number},              //联系电话
 	    email: {type:String}
-	},
-	category:{           //类别表的基本属性字段     主要指手机、U盘移动电源之类的
+	}, 
+	category:{           //类别表的基本属性字段     主要指手机、U盘移动电源之类的     这张表主要用于导航栏的分类的显示
 		cName: {type:String}
 	},
 	goods:{                 //商品的基本属性字段
-		gName: {type:String},
-		gPrice: {type:Number},
-		gDescription: {type:String},
-		gSum: {type:Number},
-		gCategory:{type:String},
-		gImg:{type:String}
+		gName: {type:String},           //商品名
+		gPrice: {type:Number},          //价格
+		gDescription: {type:String},      //商品描述
+		gSum: {type:Number},              //库存量
+		gCategory:{type:String},       // 商品所属的类别，如手机，u盘
+		gImg:{type:String}                //图片路径
 	},
-	cart:{            //购物车的基本属性字段
-		uid: {type:String},
-		gid: {type:String},
-		gName: { type: String },
+	cart:{            //购物车的基本属性字段     包含所有用户的购物车中的商品
+		uid: {type:String},            //基本和goods表的属性差不多
+		gid: {type:String},      
+		gName: { type: String },        
         gPrice: { type: String },
         gImgSrc: { type:String },
-        gQuantity: { type: Number },
-        gState:{type:Boolean,default:false}
+        gQuantity: { type: Number },    //添加到购物车中的商品数量
+        gState:{type:Boolean,default:false}           //商品的状态
 	},
 	orders:{            //订单的基本属性
 		uid: {type:String},
 		oDate: {type:Date},
-        oCategory: { type:[
+        oCategory: { type:[        //订单的内容，类型:数组 ，指包含哪些商品 
         	{
-        		gid:{type:String},
+        		gid:{type:String},       //主要包含购物车中的字段属性
         		gName:{type:String},
         		gPrice:{type:Number},
         		gImgSrc: { type:String },
                 gQuantity: { type: Number },
         	}
         ]},
-        state: {type:Boolean,default:false}
+        state: {type:Boolean,default:false}       //订单状态  
 	}
 }
